64b52a4c0099e0b063c6449caf6a09cd2c0868f3
Prerak-Home <prerak.mahajan@gmail.com>
2019-04-20 17:05:20 +0530

    Final Chnges on Dataframe

---
+++ streamingpkg/main.py
@@ -0,0 +1,17 @@
+from streamingpkg.youtubeprocessing import *
+
+
+def main():
+    print("Start of Main")
+
+    SearchKeyword="Fintech"
+
+    getyoutubecaptions(SearchKeyword)
+    print("\nAccessing from MAIN\n")
+    print("\nVideoMetaDataDF : \n",videoMetaDatadf)
+    print("\nCaption DF: \n",Captiondf)
+
+
+if __name__ == "__main__":
+    main()
+
