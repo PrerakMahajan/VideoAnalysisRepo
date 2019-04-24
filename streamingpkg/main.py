7a7f849471562930f46f5631b6d1b7aba93c8d07
prerak.mahajan@gmail.com <GitHub@123>
2019-04-24 15:49:12 +0530

    Changes in Date ,VID

--- streamingpkg/main.py
+++ streamingpkg/main.py
@@ -1,15 +1,23 @@
 from streamingpkg.youtubeprocessing import *
+# from sklearn.svm import SVC
+
 
 
 def main():
+
+
+
     print("Start of Main")
 
-    SearchKeyword="Fintech"
+
+    SearchKeyword="Fintech Issues"
 
     getyoutubecaptions(SearchKeyword)
     print("\nAccessing from MAIN\n")
     print("\nVideoMetaDataDF : \n",videoMetaDatadf)
-    print("\nCaption DF: \n",Captiondf)
+    videoMetaDatadf.to_csv("C:\\Users/sourabh.vermaLA/Desktop/Metdfff.csv")
+    Captiondf.to_csv("C:\\Users/sourabh.vermaLA/Desktop/youtube/Capf.csv")
+    print("\nCaption DF : \n",Captiondf)
 
 
 if __name__ == "__main__":
